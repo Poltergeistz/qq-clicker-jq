8b3852ded7e94a09227b4d21c03b05b22306c49d
david <david.journo.31@gmail.com>
2018-05-13 22:59:47 +0200

    amélioration et ajout des phrases aléatoires

--- app.js
+++ app.js
@@ -6,19 +6,17 @@ $(document).ready(function(){
 		var count = 0;
 		var list = ["T'as pas mieux à faire ?", "Tu joues encore à ce jeu ??",
 		"Sérieusement ? Tu continues ?", "Tu fais quoi de ta vie ?", "Ok, continue a cliquer ..."];	
-		var randomList = list[Math.floor(Math.random() * list.length)];
-		console.log(randomList)
+		console.log(count);
 	
 		$('.cake').click(function(){
-			count++;
-			//console.log(count)
-			$("span").text(count);
+			count++; // incrémente à chaque click sur l'image du cake
+			console.log(count);
+			$('span').text(count); // modifie le texte compteur en prenant en compte l'incrémentation
 		});
-		if (count == 20){
-			alert("T'as pas mieux à faire ?")
-		}
-		if (count === 40){
-			alert("Sérieusement ? Tu continues ?");
+		if ((count % 20) == 0){ //Si le nombre de click est un multiple de 20 alors (ça prends 0 mais pas le reste (20,40,60, etc), je ne comprends pas)
+			var randomList = list[Math.floor(Math.random() * list.length)]; // Ma variable fait un random de l'index pour sortir une phrase du tableau list
+			console.log(randomList);
+			alert(randomList); // affiche une phrase au hasard
 		}
 	
 	});
@@ -36,7 +34,7 @@ $(document).ready(function(){
 		if (count === 40){
 			alert("Sérieusement ? Tu continues ?");
 		}
-		for (i=0;i>0;i++){ -- premier essais
+		for (count=0;count>19;count++){ -- premier essais
 			$('span').text(count)
 		$('.counter').append(count) 
 	})
